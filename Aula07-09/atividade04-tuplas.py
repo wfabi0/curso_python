8be27d684c951d0b525f1088fb46c02d01f29537
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2025-10-23 21:05:32 -0300

    adiciona funções q11 e q12 para manipulação de tuplas na atividade04

---
+++ Aula07-09/atividade04-tuplas.py
@@ -0,0 +1,11 @@
+def q11():
+    tupla = (1, 2, 3, 4, 5)
+    print(tupla)
+
+
+def q12():
+    tupla = (
+        "Capelinha", "Ipatinga", "Rio de Janeiro",
+        "Guanhães", "Belo Horizonte"
+    )
+    print(tupla[0], tupla[4])
